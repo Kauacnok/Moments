11bcafebbae4a0ca8c74a8b671a85bf816fc770c
Kauacnok <kauaprofessionalcontact@gmail.com>
2023-04-03 22:52:30 -0300

    initial structure and styling of the header and footer of the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+import { HomeComponent } from './pages/home/home.component'
+import { AboutComponent } from './pages/about/about.component'
+import { NewMomentComponent } from './pages/new-moment/new-moment.component'
+
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'moments/new', component: NewMomentComponent }
+]
 
 @NgModule({
 	declarations: [],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewMomentComponent } from './pages/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    NewMomentComponent
   ],
   imports: [
     BrowserModule,
