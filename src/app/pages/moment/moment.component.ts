b20c30994102dff121829617b6c9a48546f1e1d3
Kauacnok <kauaprofessionalcontact@gmail.com>
2023-04-07 14:16:46 -0300

    Done the logical part to delete a moment from the database

--- src/app/pages/moment/moment.component.ts
+++ src/app/pages/moment/moment.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons'
 
 import { MomentService } from 'src/app/services/moment.service'
+import { MessagesService } from 'src/app/services/messages.service'
 import { Moment } from 'src/app/interfaces/Moment'
 import { environment } from 'src/enviroments/environment'
 
@@ -18,14 +19,20 @@ export class MomentComponent implements OnInit {
 	faTimes = faTimes
 	faEdit = faEdit
 
-	constructor(private momentService: MomentService, private route: ActivatedRoute) {}
+	constructor(private momentService: MomentService, private route: ActivatedRoute, private messagesService: MessagesService, private router: Router) {}
 
 	ngOnInit() {
 		const id = Number(this.route.snapshot.paramMap.get('id'))
 
 		this.momentService.getMoment(id).subscribe((item) => {
 			this.moment = item.data
-			console.log(this.moment)
 		})
+	}
+
+	async removeHandler(id: number) {
+		await this.momentService.deleteMoment(id).subscribe()
+
+		this.messagesService.add('Momento excluído com sucesso!')
+		this.router.navigate(['/'])
 	}
 }
